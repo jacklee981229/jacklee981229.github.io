2b6483ad9bf235c306f35463976305e314157092
jacklee981229 <jiunyihleeforwork@gmail.com>
2023-03-09 23:35:15 +0800

    Site updated: 2023-03-09 23:35:15

--- source/javascript/custom-homepage.js
+++ source/javascript/custom-homepage.js
@@ -4,6 +4,6 @@ window.onload = function(){
     document.getElementById('center-div')
     .addEventListener('click', function (event) {
         // do something
-        window.location.pathname += "/blog/";
+        window.location.pathname += "writing/";
     });
 }
